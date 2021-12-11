49293a37e4560a2d083644079a8356daa8467815
juan01-ctrl <juanierace2@gmail.com>
2021-12-11 19:56:39 -0300

    update

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,13 +1,14 @@
 const sliderShow = document.querySelector(".eg-gallery__nav");
 
-const galleryVideos = document.querySelector(".gallery");
-const galleryPhotos = document.querySelector(".gallery1");
+const galleryVideos = document.querySelector(".gallery-videos");
+const galleryPhotos = document.querySelector(".gallery-photos");
 
 sliderShow.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("nav_gallery-photos") &&
     galleryPhotos.classList.contains("none")
   ) {
+    // Swiper.update()
     galleryPhotos.classList.remove("none");
     galleryVideos.classList.add("none");
   }
